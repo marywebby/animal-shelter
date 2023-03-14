c217b3469e9649a66348fc967879b67248739040
Corbin <corbin.johns.cj@gmail.com>
2023-03-13 20:30:48 -0400

    pls pls work

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # animal-shelter
+(:)
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const animalRoutes = require('./animal-routes');
 
-router.use('/user', userRoutes);
-router.use('/animal', animalRoutes);
+const userRoutes = require('./user-routes.js');
+
+router.use('/users', userRoutes);
 
 module.exports = router;
