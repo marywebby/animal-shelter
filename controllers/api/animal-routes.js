d9926cf59e19d0b621eaf1d4f9c721574125619a
Mary Webby <MaryWebby@Marys-MacBook-Air-3.local>
2023-03-14 19:45:16 -0400

    Animal http requests

--- controllers/api/animal-routes.js
+++ controllers/api/animal-routes.js
@@ -1,23 +1,117 @@
 const router = require('express').Router();
 const { Animal } = require('../../models');
 
-router.get('/', async (req, res) => {
-  
-    try {
-      const testData = await Animal.findAll({
-        where: {
-          name: req.name,
-        },
-      }); 
-  
-      if (!testData) {
-        res.status(404).json({ message: 'Cannot find animals!' });
-        return;
-      }
-  
-      res.status(200).json(testData);
-    } catch (err) {
-      res.status(500).json(err);
+
+// ROUTE FOR GETTING ONLY CATS 
+
+router.get('/cats', async (req, res) => {
+  try {
+    const onlyCats = await Animal.findAll({
+      where: {
+        animal: false,
+      },
+    }); 
+
+    if (!onlyCats) {
+      res.status(404).json({ message: 'Cannot find cats!' });
+      return;
     }
-  
-  });
+
+    res.status(200).json(onlyCats);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// ROUTES FOR GETTING ONLY DOGS
+
+router.get('/dogs', async (req, res) => {
+  try {
+    const onlyDogs = await Animal.findAll({
+      where: {
+        animal: true,
+      },
+    }); 
+
+    if (!onlyDogs) {
+      res.status(404).json({ message: 'Cannot find cats!' });
+      return;
+    }
+
+    res.status(200).json(onlyDogs);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// ROUTE FOR GETTING ONLY HYPOALLERGENIC ANIMALS
+
+router.get('/hypoallergenic', async (req, res) => {
+  try {
+    const onlyHypo = await Animal.findAll({
+      where: {
+        hypoallergenic: true,
+      },
+    }); 
+
+    if (!onlyHypo) {
+      res.status(404).json({ message: 'Cannot find hypoallergenic animals!' });
+      return;
+    }
+
+    res.status(200).json(onlyHypo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// ROUTE FOR GETTING ONLY FEMALE ANIMALS 
+
+router.get('/female', async (req, res) => {
+  try {
+    const onlyFemale = await Animal.findAll({
+      where: {
+        sex: true,
+      },
+    }); 
+
+    if (!onlyFemale) {
+      res.status(404).json({ message: 'Cannot find Female animals!' });
+      return;
+    }
+
+    res.status(200).json(onlyFemale);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// MALE BUTTON 
+
+router.get('/male', async (req, res) => {
+  try {
+    const onlyMale = await Animal.findAll({
+      where: {
+        sex: false,
+      },
+    }); 
+
+    if (!onlyMale) {
+      res.status(404).json({ message: 'Cannot find Male animals!' });
+      return;
+    }
+
+    res.status(200).json(onlyMale);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// hypo = true -> yes
+// hypo = false -> no
+// animal = true -> dog
+// animal = false -> cat
+// sex = true -> female
+// sex = false -> male
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const animalRoutes = require('./animal-routes')
 
 router.use('/users', userRoutes);
+router.use('/animal', animalRoutes)
 
 module.exports = router;
