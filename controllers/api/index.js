4ff8c94e6ae5ced2b6363b8a6f986d9da60679f2
Corbin <corbin.johns.cj@gmail.com>
2023-03-13 20:15:36 -0400

    really fixed errors

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
 
 router.use('/users', userRoutes);
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
 
 module.exports = router;
