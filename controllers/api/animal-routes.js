7d42faba4776a27141120ba39eb42d56038261aa
Corbin <corbin.johns.cj@gmail.com>
2023-03-14 21:57:44 -0400

    missing animals now display!

--- controllers/api/animal-routes.js
+++ controllers/api/animal-routes.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
 const { Animal } = require('../../models');
 
+// ROUTE FOR ALL ANIMALS
+router.get('/', async (req, res) => {
+  try {
+    const allAnimals = await Animal.findAll({
+    }); 
+
+    if (!allAnimals) {
+      res.status(404).json({ message: 'Cannot find cats or dogs!' });
+      return;
+    }
+
+    res.status(200).json(allAnimals);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // ROUTE FOR GETTING ONLY CATS 
 
@@ -102,6 +118,25 @@ router.get('/male', async (req, res) => {
     }
 
     res.status(200).json(onlyMale);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/isMissing', async (req, res) => {
+  try {
+    const isMissing = await Animal.findAll({
+      where: {
+        isMissing: true,
+      },
+    }); 
+
+    if (!isMissing) {
+      res.status(404).json({ message: 'Cannot find missing animals!' });
+      return;
+    }
+
+    res.status(200).json(isMissing);
   } catch (err) {
     res.status(500).json(err);
   }
